ba80d3e7dc60c6ac008df9d1bb53f41c2835c8db
Theis Grønbech Petersen <theisgroenbech@pm.me>
2020-08-27 15:20:11 +0200

    Update building to include linear logic

--- src/main.ml
+++ src/main.ml
@@ -1,12 +1,11 @@
 open Pitypes
 open Parsing_helper
+open LlParser
 
 let readFile filename =
   let pi_state0 = Pitsyntax.parse_file filename in
-  ignore (Printf.printf "Debug info");
-  pi_state0
+  parse(pi_state0)
   (* parse pi_state0 *)
 
 
-let main =
-  readFile ((Sys.argv.(1)))
+let main = readFile ((Sys.argv.(1)))
