dcbfa95f7ab0596d2bee869fa6488ad99080c42b
Theis Grønbech Petersen <theisgroenbech@pm.me>
2020-08-29 14:16:43 +0200

    Add ability to parse functions

--- src/rust/rustParser.ml
+++ src/rust/rustParser.ml
@@ -10,13 +10,59 @@ let fixed_or_renamable f =
     Fixed s -> s
     | Renamable r -> r.orig_name
 
+let rec createArguments f =
+  match f with
+  t::tl -> t.tname :: (createArguments tl)
+  | _ -> []
+
+let translateEquation eq ttl =
+  let params = createArguments((fst eq.f_type))  in
+  let result = (snd(eq.f_type)).tname in
+  Func(params, fixed_or_renamable eq.f_name , result)
+
+let translateConsts f =
+  match f.f_cat with
+    Red (r::rs) -> []
+  | Eq ttl->
+    if((List.length (fst f.f_type)) = 0) then
+      [f.f_name]
+    else []
+  | _ -> []
+
+let translateFuncs f =
+  match f.f_cat with
+  Eq ttl-> translateEquation f ttl
+  | _ -> Garbage
+
+let rec cleanFunctions tf =
+  match tf with
+  | t::ts when (fixed_or_renamable t.f_name) = "||" || (fixed_or_renamable t.f_name) = "true" || (fixed_or_renamable t.f_name) = "not" || (fixed_or_renamable t.f_name) = "false" || (fixed_or_renamable t.f_name) = "&&" ->
+              (cleanFunctions ts)
+  | t::ts -> t::(cleanFunctions ts)
+  | [] -> []
+
+let rec cleanFuncs tf =
+  match tf with
+  | (Func(a,b,c))::ts -> (Func(a,b,c)) :: cleanFuncs ts
+  | _::ts -> (cleanFuncs ts)
+  | [] -> []
+
 let parse state =
   match state.pi_process_query with
     SingleProcessSingleQuery(p, _) | SingleProcess(p,_) ->
-    let frees = (List.map(fun f -> (f.f_name, (snd f.f_type).tname)) (List.rev state.pi_freenames)) in
-    let env = List.map (fun (s,t) -> FreeVar((String.lowercase (fixed_or_renamable s)),(String.capitalize t))) frees in
 
-    printFreeVars env;
+    (* Free Variables *)
+    let frees = (List.map(fun f -> (f.f_name, (snd f.f_type).tname)) (List.rev state.pi_freenames)) in
+    let env = List.map (fun (s,t) -> FreeVar(((fixed_or_renamable s)),(t))) frees in
+
+    (* Functions *)
+    let ll = (cleanFunctions state.pi_funs) in
+    let funcs = List.map (fun f -> translateFuncs f) ll in
+    let cleanFuncs = cleanFuncs funcs in
+
+    printStructs env;
+    printFuncs cleanFuncs;
+    printMain env;
 
     Printf.printf "\n";
   | _ -> ()
