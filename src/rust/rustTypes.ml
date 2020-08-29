e848119d53d7a0632204c3380c458f2b86e96aed
Theis Grønbech Petersen <theisgroenbech@pm.me>
2020-08-29 14:13:08 +0200

    Add function to rust types

--- src/rust/rustTypes.ml
+++ src/rust/rustTypes.ml
@@ -1,4 +1,6 @@
 (* Terms are either variables, represented as deBrujin indexes or
    functions *)
-type term =
+type rust =
+  | Garbage
   | FreeVar of string * string
+  | Func of string list * string * string
